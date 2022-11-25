3447582e861a4bbc6da8cb589efd9c3f79f0a2ca
Arif Maniyar <52987670+arifmaniyar0@users.noreply.github.com>
2022-11-25 18:41:11 +0530

    changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const https = require('https');
 var { graphql, buildSchema } = require('graphql');
 var { graphqlHTTP } = require('express-graphql');
 // const mongoose = require('mongoose')
-
+const axios = require('axios')
+const PaytmChecksum = require('paytmchecksum')
 const Student = require('./Models/StudentModel')
 const Employees = require('./Models/EmployeeModel')
 
@@ -21,6 +23,24 @@ type User {
   updatedAt: String
   createdAt: String
 }
+type Department {
+  _id: ID
+  departmentName: String
+}
+
+type Grade {
+  _id: ID
+  grade: String
+  year: String 
+}
+
+type Employee {
+  _id: ID!
+  name: String
+  salary: String
+  department: Department
+  grade: [Grade]
+}
 
 input UserInput {
   branch: String
@@ -34,15 +54,14 @@ type Mutation {
 }
 
 type Query {
+  Employees: [Employee]
+  TotalEmployees: Int!
+  EmployeeByName(name: String!): [Employee]
   UserByID(_id: ID!): User
   GetAllUsers: [User]
 }
 `);
 
-
-
-
-var db = {}
 
 var root = {
   GetAllUsers: async () => {
@@ -61,6 +80,18 @@ var root = {
   updateUser: async ({_id, input}) => {
     var student = await Student.updateOne({ _id }, input)
     return student
+  },
+  Employees: async () => {
+    let employees = await Employees.find();
+    return employees;
+  },
+  TotalEmployees: async () => {
+    let total = await Employees.find().count();
+    return total;
+  },
+  EmployeeByName: async (name) => {
+    let employee = await Employees.find({ name: { $regex: `.*${name}*.` } })
+    return employee;
   }
 };
 
@@ -77,12 +108,10 @@ const auth = (req, res, next) => {
   }
 
   next();
-
-  
 }
 
 Student.watch().on('change', data => console.log('change stream', data))
-Student.watch().on('error', data => console.log('error', data))
+// Student.watch().on('error', data => console.log('error', data))
 
 app.use('/', graphqlHTTP({
     schema: schema,
@@ -156,14 +185,224 @@ app.post('/test', async (req, res) => {
     'grade.grade': 1,
     'grade.year': 1,
     'departmentName': '$department.departmentName',
-  })
+  }).sort({ name: 1 })
   res.send(result)
 })
 
-app.listen(3000, () => {
+
+app.post('/offer', async (req, res) => {
+  try{
+  var paytmParams = {};
+
+  paytmParams.body = {
+      "mid"  : "iivZhi52118678221533"
+  };
+
+/*
+* Generate checksum by parameters we have in body
+* Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
+*/
+PaytmChecksum.generateSignature(JSON.stringify(paytmParams.body), "fQIjFPO9s3TlyzM#").then(function(checksum){
+
+    paytmParams.head = {      
+        "channelId" : "WEB",
+        "tokenType" : "CHECKSUM",
+        "token"     : checksum
+    };
+
+    var post_data = JSON.stringify(paytmParams);
+
+    var options = {
+
+        /* for Staging */
+        hostname: 'securegw-stage.paytm.in',
+        // hostname: 'securegw-stage.paytm.in',
+
+        /* for Production */
+        // hostname: 'securegw.paytm.in',
+
+        port: 443,
+        path    : '/theia/api/v1/fetchAllPaymentOffers?mid=iivZhi52118678221533',
+        method  : 'POST',
+        headers : {
+            'Content-Type'  : 'application/json',
+            'Content-Length': post_data.length
+        }
+    };
+
+    var response = "";
+    var post_req = https.request(options, function(post_res) {
+        post_res.on('data', function (chunk) {
+            response += chunk;
+        });
+
+        post_res.on('end', function(){
+            console.log('Response: ', response);
+        });
+    });
+    post_req.write(post_data);
+    post_req.end();
+});
+  }
+  catch(err) {
+    console.log(err.message)
+  }
+res.end();
+})
+
+app.post('/balance', (req, res) => {
+  try {
+    var paytmParams = {};
+
+paytmParams.body = {
+    "paymentMode" : "BALANCE",
+};
+
+paytmParams.head = {
+    "txnToken"    : "f0bed899539742309eebd8XXXX7edcf61588842333227"
+};
+
+var post_data = JSON.stringify(paytmParams);
+
+var options = {
+
+    /* for Staging */
+    hostname: 'securegw-stage.paytm.in',
+
+    /* for Production */
+    // hostname: 'securegw.paytm.in',
+
+    port: 443,
+    path: '/userAsset/fetchBalanceInfo?mid=YOUR_MID_HERE&orderId=ORDER_123456',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': post_data.length
+    }
+};
+
+var response = "";
+var post_req = https.request(options, function(post_res) {
+    post_res.on('data', function (chunk) {
+        response += chunk;
+    });
+
+    post_res.on('end', function(){
+        console.log('Response: ', response);
+    });
+});
+
+post_req.write(post_data);
+post_req.end();      
+  }
+  catch(err) {
+    console.log('err', err.message)
+  }
+  res.end();
+})
+
+app.post('/init', (req, res) => {
+  try{
+    var paytmParams = {};
+
+    paytmParams.body = {
+        "requestType"   : "Payment",
+        "mid"           : "iivZhi52118678221533",
+        "websiteName"   : "WEBSTAGING",
+        "orderId"       : "ORDERID_98765",
+        "callbackUrl"   : "http://localhost:3000/callback",
+        "txnAmount"     : {
+            "value"     : "1.00",
+            "currency"  : "INR",
+        },
+        "userInfo"      : {
+            "custId"    : "CUST_001",
+        },
+    };
+    
+    /*
+    * Generate checksum by parameters we have in body
+    * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
+    */
+    PaytmChecksum.generateSignature(JSON.stringify(paytmParams.body), "fQIjFPO9s3TlyzM").then(function(checksum){
+    
+        paytmParams.head = {
+            "signature"    : checksum
+        };
+    
+        var post_data = JSON.stringify(paytmParams);
+    
+        var options = {
+    
+            /* for Staging */
+            hostname: 'securegw-stage.paytm.in',
+    
+            /* for Production */
+            // hostname: 'securegw.paytm.in',
+    
+            port: 443,
+            path: '/theia/api/v1/initiateTransaction?mid=iivZhi52118678221533&orderId=ORDERID_98765',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': post_data.length
+            }
+        };
+    
+        var response = "";
+        var post_req = https.request(options, function(post_res) {
+            post_res.on('data', function (chunk) {
+                response += chunk;
+            });
+    
+            post_res.on('end', function(){
+                console.log('Response: ', response);
+            });
+        });
+    
+        post_req.write(post_data);
+        post_req.end();
+    })
+    .catch(err => {
+      console.log('errr', err)
+      res.json(err)
+    })
+  }
+  catch(err) {
+    console.log('err', err.message)
+    res.end();
+  }
+  // res.end();
+})
+
+app.listen(5000, () => {
     console.log('running at port 3000')
 })
 
+// app.get('/callback', (req, res) => {
+//   console.log('params',req.params)
+//   console.log('query',req.query)
+//   res.end()
+// })
+
+app.get('/test1', async (req, res) => {
+  
+  let employee = await Employees.aggregate([
+    {
+      $match: {}
+    },
+    {
+      $project : {
+        _id: 0,
+        name: 1,
+        salary: 1,
+        department: '$department.departmentName',
+        grade: '$grade.grade'
+      }
+    }
+  ])
+  res.send(employee)
+})
 
 // var employee = new Employees({
 //   name: 'Irfan',
